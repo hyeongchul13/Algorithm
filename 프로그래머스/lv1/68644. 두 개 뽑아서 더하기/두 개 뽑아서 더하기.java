feaa6b856e19514c65b136b44a7fbf3f81b618c1
hyeongchul13 <141476699+hyeongchul13@users.noreply.github.com>
2023-09-02 09:53:44 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.70 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,24 +1,25 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 class Solution {
     public int[] solution(int[] numbers) {
-         ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        
+         Set<Integer> set = new TreeSet<Integer>();
+
         for (int i = 0; i < numbers.length ; i++) {
-            for (int j = i+1; j < numbers.length ; j++) {
-                 int a = numbers[i] + numbers[j];
-                 if(list.indexOf(a)<0){
-                     list.add(a);
-                 }
+            for (int j = i+1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        int[] answer = new int[set.size()];
+        int i= 0;
+        Iterator iter = set.iterator();
+        while(iter.hasNext()){
+            answer[i] = (int)iter.next();
+            i++;
         }
-        Arrays.sort(answer);
         return answer;
     }
 }
