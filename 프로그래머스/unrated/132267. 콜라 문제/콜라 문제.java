25a94ab2b73a121b6cfa4d406d85503f8d38afd6
IfOnly <141476699+hyeongchul13@users.noreply.github.com>
2023-09-11 09:54:38 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
         
-        while(true){
-            if (n < a){ 
-                break;
-            }
-            answer += (n/a) *b; 
-            int remain = n % a; 
-
-            n = (n/a) * b + remain; 
-        }
-        
-        return answer;
+       return (n - b) / (a - b) * b;
     }
 }
