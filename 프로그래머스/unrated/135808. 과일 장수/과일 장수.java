6403639f2110a19d2b6238fe9198aca750529f63
IfOnly <141476699+hyeongchul13@users.noreply.github.com>
2023-10-09 10:00:33 +0900

    [unrated] Title: 과일 장수, Time: 0.38 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        int boxNum = score.length / m;
+        Arrays.sort(score);
+        
+         if (score.length % m == 0){
+            for (int i = 0; i < boxNum; i++) {
+                answer += (score[i * m]) * m * 1;
+            }
+        }
+        if (score.length % m != 0){
+            int rest = score.length % m;
+            for(int i = 0; i < boxNum; i++) {
+                answer += (score[rest + (i * m)]) * m * 1;
+            }
+        }
+        return answer;
+    }
+}
